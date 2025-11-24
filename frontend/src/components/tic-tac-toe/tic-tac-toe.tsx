f0eeb9e71b9b4e2a64a0a54af13f572dc89e3cf3
Sava Mihajlov <79896945+Savaxc@users.noreply.github.com>
2025-11-24 14:35:21 +0100

    Added UI board

--- frontend/src/components/tic-tac-toe/tic-tac-toe.tsx
+++ frontend/src/components/tic-tac-toe/tic-tac-toe.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import React, { useState } from 'react';
+import { Board } from '../board/board';
+import '../board/board.css';
+import './tic-tac-toe.css';
 
 export const TicTacToe = () => {
+  const [board] = useState<any>(
+    Array.from({ length: 3 }, () => Array.from({ length: 3 }, () => null))  //kreiranje table, 3x3
+  );
+
   return (
     <div className='game'>
       <h1>Tic-Tac-Toe</h1>
-      Board
+      <Board board={board} handleClick={() => ""} />
     </div>
   );
 }
