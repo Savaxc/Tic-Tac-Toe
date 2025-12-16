5141698189175ba367cd7d96b67ecfc35cb221f0
Sava Mihajlov <79896945+Savaxc@users.noreply.github.com>
2025-12-16 22:22:36 +0100

    Upgraded AI Moves

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { TicTacToe } from "./components/tic-tac-toe/tic-tac-toe";
+import { AITicTacToe } from "./components/tic-tac-toe/tic-tac-toe";
 import {
   BrowserRouter as Router,
   Routes,
@@ -34,7 +34,7 @@ function App() {
         <main>
           <Routes>
             <Route path="/" element={<HomePage />} />
-            <Route path="/single-player" element={<TicTacToe />} />
+            <Route path="/single-player" element={<AITicTacToe />} />
 
             <Route
               path="/multiplayer"
